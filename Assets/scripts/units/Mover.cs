2f849bfc2d9c1ca67bf9bfe34059e4719470c221
ReganUA <162838412+ReganUA@users.noreply.github.com>
2026-02-10 09:21:20 +0100

    initialCommit

---
+++ Assets/scripts/units/Mover.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+public class Mover : MonoBehaviour
+{
+    private Rigidbody rb;
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void MoveObject(Vector3 direction, float speed)
+    {
+        rb.linearVelocity = speed * direction;
+    }
+    public void RotateObject(Transform obj, Vector3 targetDir, float speed)
+    {
+        if (targetDir == Vector3.zero) return;
+
+        targetDir.y = 0;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDir);
+        obj.rotation = Quaternion.RotateTowards(obj.rotation, targetRotation, speed * Time.deltaTime);
+    }
+    public void MoveObject(Vector3 direction, float speed, float acceleration)
+    {
+        // 1. Обчислюємо цільову швидкість тільки для горизонтальних осей (X, Z)
+        Vector3 targetHorizontalVelocity = direction * speed;
+
+        // 2. Беремо поточну швидкість Rigidbody
+        Vector3 currentVelocity = rb.linearVelocity;
+
+        // 3. Плавно змінюємо тільки горизонтальну складову
+        Vector3 newHorizontalVelocity = Vector3.MoveTowards(
+            new Vector3(currentVelocity.x, 0, currentVelocity.z),
+            targetHorizontalVelocity,
+            acceleration * Time.fixedDeltaTime
+        );
+
+        // 4. Повертаємо оригінальний Y (гравітацію) назад у вектор
+        rb.linearVelocity = new Vector3(newHorizontalVelocity.x, currentVelocity.y, newHorizontalVelocity.z);
+    }
+    public void RotateObject(Transform obj, Vector3 targetDir, float speed, float smoothAmount)
+    {
+        if (targetDir == Vector3.zero) return;
+
+        targetDir.y = 0;
+        Quaternion targetRotation = Quaternion.LookRotation(targetDir);
+        obj.rotation = Quaternion.Slerp(obj.rotation, targetRotation, speed * Time.deltaTime * smoothAmount);
+    }
+}
---
+++ Assets/scripts/units/player/playerController.cs
@@ -0,0 +1,30 @@
+﻿using TreeEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class playerController : Tank
+{
+    public InputActionReference move;
+    private void Update()
+    {
+        Vector2 dir = move.action.ReadValue<Vector2>();
+        Ride(new Vector3(dir.x, 0, dir.y));
+
+        AimTurret();
+    }
+    private void AimTurret()
+    {
+        Vector2 mousePosition = Mouse.current.position.ReadValue();
+        Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, LayerMask.GetMask("Ground")))
+        {
+            // 2. Отримуємо напрямок до точки зіткнення
+            Vector3 lookDir = hit.point - Turret.transform.position;
+            lookDir.y = 0; // Щоб башта не нахилялася в землю
+            if (lookDir != Vector3.zero)
+            {
+                 RotateTurret(lookDir);
+            }
+        }
+    }
+}
